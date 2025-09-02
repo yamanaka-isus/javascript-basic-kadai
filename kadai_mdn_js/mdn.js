8734e0b9ce9d2679cd2c0c9e822f40de49412fe1
yamanaka-isus <yamanaka@i-sus.com>
2025-09-02 17:22:00 +0900

    Update mdn.js
    ①get.FullYearとMonth、Dayの使用
    ②toLocaleDateStringの使用

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,9 +1,13 @@
-const event = new Date(Date.UTC(2025, 8, 2, 3, 0, 0));
+const event = new Date();
+const year = event.getFullYear();
+const month = event.getMonth();
+const day = event.getDay();
+
 const options = {
-  weekday: "long",
   year: "numeric",
   month: "long",
   day: "numeric",
 };
 
 console.log(event.toLocaleDateString(undefined, options));
+// 期待される結果（地方時と既定のロケールによって変化）: 2012年12月20日木曜日
